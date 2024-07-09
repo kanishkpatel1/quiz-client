bb7097280cb4bfa5d2369337c51f6877a9b4d80e
Kanishk Patel <100139582+kanishkpatel1@users.noreply.github.com>
2024-07-09 08:43:31 +0530

    Update index.js

--- src/apicalls/index.js
+++ src/apicalls/index.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
+    baseURL: process.env.REACT_APP_API_BASE_URL,
     headers: {
-         Authorization : `Bearer ${localStorage.getItem('token')}`
+        Authorization: `Bearer ${localStorage.getItem('token')}`
     }
 });
 
